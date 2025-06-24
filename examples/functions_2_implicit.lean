c8d16db9f90ce43f2f573045eaf8a2cbf38eb2d5
Moritz Grimm <info@moritz-grimm.com>
2025-06-24 13:25:36 +0200

    Fixes for implicit parameters

--- examples/functions_2_implicit.lean
+++ examples/functions_2_implicit.lean
@@ -1,6 +1,6 @@
 prelude
 
 def comp : {A : Type} → {B : Type} → {C : Type} → (f : A → B) → (g : B → C) → (A → C) :=
-   λ f ↦ λ g ↦ λ a ↦ (g (f a))
+   λ {A} ↦ λ {B} ↦ λ {C} ↦ λ f ↦ λ g ↦ λ a ↦ (g (f a))
 
 def example_application (A : Type) (f : A → A) : A → A := comp f f
